783650368616d0abdd96a17d62c4c3ba5af1d0ff
Victor Fernandes <victor.santos136@fatec.sp.gov.br>
2023-09-11 21:44:04 -0300

    finalized menu structure

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,49 +1,18 @@
 <template>
   <v-app>
-    <!-- Componente TopBar fixo no topo -->
     <TopBar class="fixed-top" />
-
     <v-main>
-      <v-row>
-        <v-col cols="3">
-          <!-- Componente SideBar -->
-          <SideBar class="mt-12" />
-        </v-col>
-
-        <v-col cols="9">
-          <!-- Conteúdo principal do aplicativo -->
-          <router-view />
-        </v-col>
-      </v-row>
+        <!-- Conteúdo principal vai aqui -->
     </v-main>
   </v-app>
 </template>
 
-<script>
-import SideBar from "@/components/SideBar"; // Importe o componente SideBar
+<script> // Importe o componente SideBar
 import TopBar from "@/components/TopBar";   // Importe o componente TopBar
 
 export default {
-  components: {
-    SideBar, // Registre o componente SideBar
+  components: { // Registre o componente SideBar
     TopBar,  // Registre o componente TopBar
   },
 }
 </script>
-
-<style scoped>
-.fixed-top {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-}
-.mt-12 {
-  margin-top: 12px;
-}
-
-/* Define um z-index menor para o SideBar */
-.v-navigation-drawer {
-  z-index: 1000;
-}
-</style>
