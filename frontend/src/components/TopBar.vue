81fbb4e249d9184613372b296793abc0de263beb
Aldrik Alvaro <aldrikfariaalvaro@gmail.com>
2023-09-22 19:52:57 -0300

    KAN-33
    finalizacao tela de ingestao

--- frontend/src/components/TopBar.vue
+++ frontend/src/components/TopBar.vue
@@ -29,6 +29,7 @@
         <v-icon>mdi-account-circle-outline</v-icon>
       </v-btn>
        </v-app-bar>
+   
     </div>
      
     <div class="container">
@@ -43,19 +44,21 @@
         >
           <v-list nav dense>
             <v-list-item link @click="selectItem('dashboard')" :class="{ 'selected-item': selectedItem === 'dashboard' }">
-              <v-list-item-icon>
+              <v-list-item-icon  style="justify-content: center; align-items: center;">
                 <div class="indicator" v-if="selectedItem === 'dashboard'"></div>
-                <v-icon>mdi-view-dashboard</v-icon>
+                <i class="fa fa-bar-chart" aria-hidden="true" style="color: #757575; font-size: 22px; "></i>
               </v-list-item-icon>
               <v-list-item-title>Dashboard</v-list-item-title>
             </v-list-item>
 
             <v-list-item link @click="selectItem('ingestao')" :class="{ 'selected-item': selectedItem === 'ingestao' }">
-              <v-list-item-icon>
+              <v-list-item-icon  style="justify-content: center; align-items: center;">
+             
                 <div class="indicator" v-if="selectedItem === 'ingestao'"></div>
-                <v-icon>mdi-view-dashboard</v-icon>
+                <i class="fa fa-download" aria-hidden="true" style="color: #757575; font-size: 22px; "></i>
+              
               </v-list-item-icon>
-              <v-list-item-title>Ingestão</v-list-item-title>
+              <v-list-item-title>Upload</v-list-item-title>
             </v-list-item>
           </v-list>
         </v-navigation-drawer>
@@ -63,8 +66,8 @@
       </div>
 
       <div class="conteudo-dash" v-if="selectedItem !== 'ingestao'">
-          <!-- <oracle-dv project-path="/@Catalog/users/aldrik.alvaro@fatec.sp.gov.br/Novo Arquivo de Trabalho" active-page="canvas" active-tab-id="1">
-        </oracle-dv>  -->
+        <oracle-dv project-path="/@Catalog/users/aldrik.alvaro@fatec.sp.gov.br/Novo Arquivo de Trabalho" active-page="canvas" active-tab-id="1">
+        </oracle-dv>  
       </div>
 
       <div class="conteudo-ingestao" v-if="selectedItem === 'ingestao'">
@@ -77,9 +80,7 @@
               <label for="csvFile" class="file-upload-button">Select CSV File</label> -->
               <input type="file" id="csvFile" accept=".csv" ref="fileInput" @change="updateSelectedFileName" style="display: none;" >
               <label for="csvFile" class="file-upload-button"><i class="fa fa-file-text" aria-hidden="true" style="font-size: 54px; color: #222222;"></i></label>
-              
               <span class="selected-file-name">{{ selectedFileName }}</span>
-              
               <!-- <label for="csvFile">Drag or select a file from your computer</label> -->
             </div>
             <br>
@@ -149,20 +150,10 @@ export default {
 
 
 <style scoped>
-.custom-top-bar {
-  height: 64px; 
-}
-.negrito-font {
-  font-size: 14px;
-  font-weight: bold;
-}
+
 .clearfix {
   clear: both; /* Limpa elementos flutuantes antes de iniciar uma nova linha */
 }
-.normal-font {
-  font-size: 14px;
-}
-
 .v-app-bar {
   position: absolute;
   top: 0;
@@ -170,11 +161,45 @@ export default {
   right: 0;
 }
 
-.selected-item {
-  background-color: #F1EFEE;
-  position: relative;
+
+/* ############################################################ */
+/* CSS - CONTEUDO */
+.container {
+  background-color: #f0efed !important;/* Cor de fundo personalizada para b */
+  display: flex;
+  flex-direction: row;
+  height: 95vh; /* Definir a altura da container para ocupar 95% da altura da janela de visualização */
+}
+.main-container {
+  background-color: #F1EFEE; /* Cor de fundo cinza */
+  padding: 20px; /* Espaçamento interno para o conteúdo */
 }
 
+
+/* ############################################################ */
+/* CSS - MENU HORIZONTAL */
+.menuHorizontal{
+  border: 5px solid red; 
+  width: 100%; 
+  height: 5%;
+}
+.normal-font {
+  font-size: 14px;
+}
+.custom-top-bar {
+  height: 64px; 
+}
+.negrito-font {
+  font-size: 14px;
+  font-weight: bold;
+}
+
+/* ############################################################ */
+/* CSS - MENU VERTICAL */
+.menuVertical {
+  margin-top: 10px;
+  background-color: #fafafa !important; /* Cor de fundo personalizada para c */
+}
 .indicator {
   width: 4px;
   background-color: #E90505; 
@@ -183,63 +208,14 @@ export default {
   left: 0;
   bottom: 0;
 }
-
-.main-container {
-  background-color: #F1EFEE; /* Cor de fundo cinza */
-  padding: 20px; /* Espaçamento interno para o conteúdo */
-}
-
-.container {
-  background-color: #f0efed !important;/* Cor de fundo personalizada para b */
-  display: flex;
-  flex-direction: row;
-  height: 95vh; /* Definir a altura da container para ocupar 95% da altura da janela de visualização */
-}
-
-.conteudo-dash {
-  border: 5px solid rgb(39, 149, 182); 
-  background-color: #CCCCFF !important;/* Cor de fundo personalizada para b */
-  width: 5%;
-  flex-grow: 1;
-  box-sizing: border-box;
+.selected-item {
+  background-color: #F1EFEE;
   position: relative;
-  padding-top: 15px;
-
 }
-
-
-
-
-
-
-.menuVertical {
-  background-color: #FFCCCC !important; /* Cor de fundo personalizada para c */
-}
-
-
-
-.menuHorizontal{
-  border: 5px solid red; 
-  width: 100%; 
-  height: 5%;
-}
-
-
 
 /* ############################################################ */
-.ingestao-btn{
-  /* border: 5px solid rgb(192, 44, 137); */
-  padding-top: 10px;
-  padding-bottom: 10px;
-  padding-left: 35px;
-  padding-right: 35px;
-  margin-left: 46%;
-  border-radius: 50px;
-  background-color: #E90505; 
-  color: white;
-}
-
-.conteudo-ingestao {
+/* CSS - TELA BI */
+.conteudo-dash {
   border: 5px solid #ffffff !important;
   background-color: #ffffff !important;
   border-radius: 30px;
@@ -249,38 +225,69 @@ export default {
   box-sizing: border-box;
   position: relative;
   padding-top: 15px;
-
+  padding-bottom: 15px;
+  /* width: 5%;
+  flex-grow: 1;
+  box-sizing: border-box;
+  position: relative;
+  padding-top: 15px; */
 }
 
-.ingestao-upload {
-  border: 5px dotted rgb(57 62 59);
-  background-color: #f0efed;
-  border-radius: 10px;
-  display: flex;
-  flex-direction: column; /* Altera a direção da flexbox para column */
-  justify-content: center; /* Centraliza verticalmente */
-  align-items: center; /* Centraliza horizontalmente */
-  height: 20vh;
-  margin-left: 30%;
-  margin-right: 30%;
-  margin-top: 15%;
-}
+/* ############################################################ */
+/* CSS - TELA INGESTAO */
+  .ingestao-btn{
+    /* border: 5px solid rgb(192, 44, 137); */
+    padding-top: 10px;
+    padding-bottom: 10px;
+    padding-left: 35px;
+    padding-right: 35px;
+    margin-left: 46%;
+    border-radius: 50px;
+    background-color: #E90505; 
+    color: white;
+  }
+  .conteudo-ingestao {
+    border: 5px solid #ffffff !important;
+    background-color: #ffffff !important;
+    border-radius: 30px;
+    margin: 20px;
+    width: 5%;
+    flex-grow: 1;
+    box-sizing: border-box;
+    position: relative;
+    padding-top: 15px;
 
-.ingestao-upload label {
-  margin-top: 10px; /* Espaço entre o campo de entrada e o rótulo */
-}
+  }
+  .ingestao-upload {
+    border: 5px dotted rgb(57 62 59);
+    background-color: #f0efed;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column; /* Altera a direção da flexbox para column */
+    justify-content: center; /* Centraliza verticalmente */
+    align-items: center; /* Centraliza horizontalmente */
+    height: 20vh;
+    margin-left: 30%;
+    margin-right: 30%;
+    margin-top: 15%;
+  }
+  .ingestao-upload label {
+    margin-top: 10px; /* Espaço entre o campo de entrada e o rótulo */
+  }
+  .file-upload-button {
+    cursor: pointer;
+    padding: 10px 20px;
+    /* background-color: #007bff; */
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+  }
+  .file-upload-button:hover {
+    background-color: #cdcdcd;
+  }
+  .ingestao-btn:hover {
+    background-color: #ec4040; 
+  }
 
-.file-upload-button {
-  cursor: pointer;
-  padding: 10px 20px;
-  /* background-color: #007bff; */
-  color: #fff;
-  border: none;
-  border-radius: 5px;
-}
-
-.file-upload-button:hover {
-  background-color: #cdcdcd;
-}
 
 </style>
