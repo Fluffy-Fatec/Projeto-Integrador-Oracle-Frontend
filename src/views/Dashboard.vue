b6d3d641a9c7454a808d04c0f495632724bfb314
Victor Fernandes <victor.santos136@fatec.sp.gov.br>
2023-09-24 21:05:49 -0300

    Alteracoes de styles para melhor alocacao do BI

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,7 +3,7 @@
   <menuVertical />
   <div class="fundoDashboard">
       <div class="conteudoDashboard">
-        <oracle-dv project-path="/@Catalog/users/aldrik.alvaro@fatec.sp.gov.br/Oracle-Dinner" active-page="canvas" active-tab-id="1">
+        <oracle-dv project-path="/@Catalog/users/aldrik.alvaro@fatec.sp.gov.br/Oracle-Dinner" active-page="insight" active-tab-id="snapshot!canvas!1">
         </oracle-dv>
       </div>
   </div>
@@ -24,18 +24,8 @@
 }
 .conteudoDashboard {
   /* border: 5px solid red; width: 100%; height: 100%; */
-  border: 2px solid #ffffff !important;
-  background-color: #ffffff !important;
-  border-radius: 10px;
-  margin: 20px;
-  width: 5%;
   flex-grow: 1;
-  box-sizing: border-box;
   position: relative;
-  padding-top: 12px;
-  padding-bottom: 12px;
-  padding-left: 5px;
-  padding-right: 5px;
 
 }
 
