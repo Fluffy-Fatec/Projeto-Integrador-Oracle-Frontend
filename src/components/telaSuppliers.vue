553cea2387314b7df160c6383ff51515d55fd560
Aldrik Alvaro <aldrikfariaalvaro@gmail.com>
2023-09-29 21:06:50 -0300

    KAN-79

---
+++ src/components/telaSuppliers.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="fundoSuppliers">
+      <div class="conteudoSuppliers">
+        Suppliers
+
+        
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import axios from "axios";
+  export default {
+      name: 'Suppliers',
+      data() {
+      return {
+
+        };
+      },
+      methods: {
+    },
+  }
+  </script>
+
+<style scoped>
+.fundoSuppliers {
+  background-color: #f0efed !important;/* Cor de fundo personalizada para b */
+  display: flex;
+  flex-direction: row;
+  height: 95vh; /* Definir a altura da container para ocupar 95% da altura da janela de visualização */
+}
+.conteudoSuppliers {
+  /* border: 5px solid red; width: 100%; height: 100%; */
+    border: 2px solid #ffffff !important;
+    background-color: #ffffff !important;
+    border-radius: 10px;
+    margin: 15px;
+    width: 5%;
+    flex-grow: 1;
+    box-sizing: border-box;
+    position: relative;
+
+}
+</style>
+  
---
+++ src/views/Suppliers.vue
@@ -0,0 +1,11 @@
+<template>
+  <menuHorizontal />
+  <menuVertical />
+  <telaSuppliers />
+</template>
+
+<script setup>
+  import menuVertical from '@/components/menuVertical.vue'
+  import menuHorizontal from '@/components/menuHorizontal.vue'
+  import telaSuppliers from '@/components/telaSuppliers.vue'
+</script>
