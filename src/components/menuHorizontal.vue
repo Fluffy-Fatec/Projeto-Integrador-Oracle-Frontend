db2be0fee17a923bd32b98fa68078dd2bdd44e51
luizborges17 <lfborgesrp1@gmail.com>
2023-11-25 18:22:03 -0300

    KAN-150 feature: criando o botão e modal de notificação

--- src/components/menuHorizontal.vue
+++ src/components/menuHorizontal.vue
@@ -22,17 +22,103 @@
             <span class="normal-font mr-2">RestaurantName</span>
           </div>
           <v-spacer></v-spacer>
+          
+          <v-btn  @click="openModalNotification">
+            <i class="fa fa-bell" aria-hidden="true" style="color: #ffffff; font-size: 20px; "></i>          
+          </v-btn>
+      
           <v-btn target="_blank" text>
             <i class="fa fa-user-circle-o" aria-hidden="true" style="color: #ffffff; font-size: 20px; "></i>
           
           </v-btn>
     </v-app-bar>
   </div>
+
+  <v-dialog v-model="modalNotify" persistent width="512">
+    <v-card>
+
+      <v-card-title>
+        <span class="text-h5">Notifications</span>
+      </v-card-title>
+
+      <v-card-text >
+        <v-container>
+          <v-row>
+            <v-list>
+              <v-list-item-group v-if="notificationsss.length > 0">
+                <v-list-item
+                  v-for="notification in notificationsss"
+                  :key="notification.id_notification"
+                  @click="listarNotifications"
+                >
+                  <v-list-item-content>
+                    <v-list-item-title>{{ notification.name }}</v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list-item-group>
+              <v-list-item v-else>
+                <v-list-item-content>Nenhuma notificação disponível</v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-row>
+        </v-container>
+      </v-card-text>
+
+      <!-- FOOTER -->
+      <v-card-actions class="text-center">
+        <v-spacer></v-spacer>
+        <button class="Delete-btn" @click="modalNotify = false">Close</button>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
-<script setup>
-  //
+<script>
+  import axios from "axios";
+  export default {
+
+    name: 'Product',
+    data() {
+    return {
+      elected: '',
+      data: [],
+      notificationsss: [
+        { id_notification: 1, name: 'Notificação A' },
+        { id_notification: 2, name: 'Notificação B' },
+        { id_notification: 3, name: 'Notificação C' },
+      ],
+      modalNotify: false,
+      isFieldDisabled: true, // Defina o campo como desabilitado inicialmente
+      modalOpen: false,
+      isMobile: window.innerWidth <= 700,
+      dialog: false,
+      select: null,
+
+      };
+    },
+    created() {
+        this.listarNotifications();
+    },
+
+    methods: {
+      listarNotifications(){
+        axios.get('api/notificacao')
+          .then(response => {
+            // Extract the "name" properties from the response and store them in this.suppliers
+            this.notificationsss = response.data.map(notification => notification.name);
+            console.log(this.notificationsss)
+          })
+          .catch(error => {
+            console.error('Error fetching data:', error);
+          });
+      },
+      openModalNotification(){
+        this.modalNotify = true;
+      },
+    },
+  }
 </script>
+
 
 <style scoped>
 /* ############################################################ */
@@ -41,6 +127,19 @@
   border: 0px solid red; 
   width: 0%; 
   height: 0%;
+}
+
+.Delete-btn{
+ background-color: 5px solid rgb(51, 51, 51); 
+padding-top: 10px;
+padding-bottom: 10px;
+padding-left: 35px;
+padding-right: 35px;
+
+/* margin-left: 46%; */
+border-radius: 50px;
+background-color: #E90505; 
+color: white;
 }
 .normal-font {
   font-size: 14px;
