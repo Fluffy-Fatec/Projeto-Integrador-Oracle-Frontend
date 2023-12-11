e0ab849b6520ab7d7fb5f696de76c254aef4acaa
heyanaleal <90358925+heyanaleal@users.noreply.github.com>
2023-12-11 17:27:53 -0300

    update

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ import { createApp } from 'vue'
 import { registerPlugins } from '@/plugins'
 
 // axios.defaults.baseURL =  'http://localhost:8080/'
-axios.defaults.baseURL =  'https://20.55.33.215:8080/'
+axios.defaults.baseURL =  'http://20.55.33.215:8080/'
 
 const app = createApp(App)
 
